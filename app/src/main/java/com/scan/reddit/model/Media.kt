7d34407ae0e8ef8dba8a2b0ab0a3e298e15c4091
mac <rehamgalal1@outlook.com>
2021-11-04 20:03:57 +0200

    "fix favourite"

--- app/src/main/java/com/scan/reddit/model/Media.kt
+++ app/src/main/java/com/scan/reddit/model/Media.kt
@@ -2,4 +2,14 @@ package com.scan.reddit.model
 
 data class Media(
     val reddit_video: RedditVideo
-)
+) {
+    companion object {
+        operator fun invoke(
+            reddit_video: RedditVideo? = null,
+        ): Media {
+            return Media(
+                reddit_video ?: RedditVideo("",0,"",0,"",false,"","",0),
+            )
+        }
+    }
+}
